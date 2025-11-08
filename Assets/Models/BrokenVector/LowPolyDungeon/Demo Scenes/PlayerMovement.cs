23e2e08273edd4c2d7acb587b37df90531f2d471
CalviceGaming <diogo.pereira.d24@gmail.com>
2025-11-08 14:54:26 +0000

    basic player movement

--- Assets/Models/BrokenVector/LowPolyDungeon/Demo Scenes/PlayerMovement.cs
+++ Assets/Models/BrokenVector/LowPolyDungeon/Demo Scenes/PlayerMovement.cs
@@ -1,17 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public CharacterController controller;
+    public InputActionAsset InputActions;
+    public Rigidbody rb;
     public float baseSpeed = 12f;
-    public float gravity = -9.81f;
-    public float jumpHeight = 3f;
-    public float sprintSpeed = 5f;
+    
+    [SerializeField] private float maxSpeed = 10f;
 
-    float speedBoost = 1f;
-    Vector3 velocity;
+    [SerializeField] private GameObject Camera; 
+    
+    private InputAction moveAction;
+    private Vector2 moveAmount;
+    private InputAction lookAction;
+    private Vector2 lookDirection;
+
+    private void OnEnable()
+    {
+        InputActions.FindActionMap("Player").Enable();
+    }
+    
+    private void OnDisable()
+    {
+        InputActions.FindActionMap("Player").Disable();
+    }
+
+    private void Awake()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        lookAction = InputSystem.actions.FindAction("Look");
+    }
+    
     void Start()
     {
 
@@ -19,31 +41,37 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        if (controller.isGrounded && velocity.y < 0)
+        moveAmount = moveAction.ReadValue<Vector2>();
+        lookDirection = lookAction.ReadValue<Vector2>();
+    }
+
+    void FixedUpdate()
+    {
+        if (maxSpeed < rb.linearVelocity.magnitude) return;
+        Move();
+        Debug.Log(rb.linearVelocity);
+    }
+
+    private void Move()
+    {
+        if (Keyboard.current.wKey.isPressed)
         {
-            velocity.y = -2f;
+            Vector3 direction = new Vector3(moveAmount.x, 0, moveAmount.y).normalized;
+            
+            rb.AddForce((Camera.transform.forward * baseSpeed * Time.deltaTime), ForceMode.VelocityChange);
+            
         }
-
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        if (Input.GetButton("Fire3"))
-            speedBoost = sprintSpeed;
-        else
-            speedBoost = 1f;
-
-
-        Vector3 move = transform.right * x + transform.forward * z;
-
-        controller.Move(move * (baseSpeed + speedBoost) * Time.deltaTime);
-
-        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        if (Keyboard.current.sKey.isPressed)
         {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            rb.AddForce((-Camera.transform.forward * baseSpeed * Time.deltaTime), ForceMode.VelocityChange);
         }
-
-        velocity.y += gravity * Time.deltaTime;
-
-        controller.Move(velocity * Time.deltaTime);
+        if (Keyboard.current.aKey.isPressed)
+        {
+            rb.AddForce((-Camera.transform.right * baseSpeed * Time.deltaTime), ForceMode.VelocityChange);
+        }
+        if (Keyboard.current.dKey.isPressed)
+        {
+            rb.AddForce((Camera.transform.right * baseSpeed * Time.deltaTime), ForceMode.VelocityChange);
+        }
     }
 }
